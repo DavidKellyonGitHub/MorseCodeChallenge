285a968ed0f84b764859d9b019059118c716f7e3
Dave Kelly <kelly.david.t@gmail.com>
2020-04-14 16:15:46 -0400

    began framework

--- src/main/java/Decoder.java
+++ src/main/java/Decoder.java
@@ -1,7 +1,34 @@
+import java.util.ArrayList;
+
 public class Decoder {
+    ArrayList<String> encodedWords = new ArrayList<>();
 
-    public static String decode (String encoded) {
-
+    public static String decode(String encoded) {
         return null;
     }
+
+    //returns dit for .
+    public Boolean isDit(String checkMeForDit) {
+        return true;
+    }
+
+    //returns dah for _
+    public boolean isDah(String checkMeForDah) {
+        return false;
+    }
+
+    //adds encoded words to field list encodedWords
+    public void identifyWords(String encoded){
+            this.encodedWords = new ArrayList<String>();
+    }
+
+    //consider using pattern matcher
+    public String identifyLetter(String patternToMatch){
+        return null;
+    }
+
+
+
+
+
 }
